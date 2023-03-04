7b32ab4945a86e5c41698b529bafa4e763d028dc
sriharitirumala <sritirumala30@gmail.com>
2023-03-04 18:11:17 +0530

    [Story-1] Adding play vars

--- ec2/main.tf
+++ ec2/main.tf
@@ -3,7 +3,7 @@ data "aws_caller_identity" "current" {}
 data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "devops-practice-with-ansible"
-  owners      = [data.aws_caller_identity.current.account_id]
+  owners      = [017745c0d893b3a6b]
 }
 
 
