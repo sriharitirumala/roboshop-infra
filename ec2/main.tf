a344698102ff07e602f139cf20fbea3d66cb965e
sriharitirumala <sritirumala30@gmail.com>
2023-03-04 15:14:02 +0530

    [Story-1] Adding play vars

--- ec2/main.tf
+++ ec2/main.tf
@@ -58,8 +58,8 @@ resource "aws_security_group" "sg" {
 }
 
 resource "aws_route53_record" "record" {
-  zone_id = "Z103214126L48SQW30RSR"
-  name    = "${var.component}-dev.devopsb71.online"
+  zone_id = "Z099374713TEKOU3HRH9T"
+  name    = "${var.component}-dev.devopsb71services.site"
   type    = "A"
   ttl     = 30
   records = [aws_instance.ec2.private_ip]
