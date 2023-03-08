2751860a454b621bdcedd3d7b0cf9dcbe4606111
sriharitirumala <sritirumala30@gmail.com>
2023-03-08 11:49:34 +0530

    [Story-1] adding correct Repos

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,21 +1,12 @@
-data "aws_caller_identity" "current" {}
-
-data "aws_ami" "ami" {
-  most_recent = true
-  name_regex  = "devops-practice-with-ansible"
-  owners      = [data.aws_caller_identity.current.account_id]
-}
-
-
 resource "aws_instance" "ec2" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg.id]
+  iam_instance_profile   = "${var.env}-${var.component}-role"
   tags = {
     Name = var.component
   }
 }
-
 
 resource "null_resource" "provisioner" {
   provisioner "remote-exec" {
@@ -27,8 +18,9 @@ resource "null_resource" "provisioner" {
     }
 
     inline = [
-      "ansible-pull -i localhost, -U https://github.com/sriharitirumala/roboshop-ansible roboshop.yml -e role_name=${var.component}"
+      "ansible-pull -i localhost, -U https://github.com/srharitirumala/roboshop-ansible roboshop.yml -e role_name=${var.component} -e env=${var.env}"
     ]
+
   }
 
 }
@@ -36,7 +28,6 @@ resource "null_resource" "provisioner" {
 resource "aws_security_group" "sg" {
   name        = "${var.component}-${var.env}-sg"
   description = "Allow TLS inbound traffic"
-
 
   ingress {
     description = "ALL"
@@ -58,20 +49,66 @@ resource "aws_security_group" "sg" {
   }
 }
 
-
 resource "aws_route53_record" "record" {
   zone_id = "Z099374713TEKOU3HRH9T"
-  name    = "${var.component}-dev.devopsb71services.site"
+  name    = "${var.component}-dev.devopdb71services.site"
   type    = "A"
   ttl     = 30
   records = [aws_instance.ec2.private_ip]
 }
 
+resource "aws_iam_policy" "ssm-policy" {
+  name        = "${var.env}-${var.component}-ssm"
+  path        = "/"
+  description = "${var.env}-${var.component}-ssm"
 
-
-variable "component" {}
-variable "instance_type" {}
-variable "env" {
-  default = "dev"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource" : "arn:aws:ssm:us-east-1:119044227534:parameter/${var.env}.${var.component}*"
+      },
+      {
+        "Sid" : "VisualEditor1",
+        "Effect" : "Allow",
+        "Action" : "ssm:DescribeParameters",
+        "Resource" : "*"
+      }
+    ]
+  })
 }
 
+resource "aws_iam_role" "role" {
+  name = "${var.env}-${var.component}-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "profile" {
+  name = "${var.env}-${var.component}-role"
+  role = aws_iam_role.role.name
+}
+
+resource "aws_iam_role_policy_attachment" "policy-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.ssm-policy.arn
+}
