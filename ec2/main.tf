fcf800ab13abe6681cb90a1c6cf790f2744500a7
sriharitirumala <sritirumala30@gmail.com>
2023-03-09 15:06:35 +0530

    [Story-1] adding correct Repos

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,9 +1,7 @@
-data "aws_caller_identity" "current" {}
-
 data "aws_ami" "ami" {
   most_recent = true
   name_regex = "devops-practice-with-ansible"
-  owners = [data.aws_caller_identity.current.account_id]
+  owners = [119044227534]
 }
 
 
