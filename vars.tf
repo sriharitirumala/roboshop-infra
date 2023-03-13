e910b1061c181eeda220ece459ad4b44e7284d64
sriharitirumala <sritirumala30@gmail.com>
2023-03-13 14:48:43 +0530

    [Story-1] adding correct Repos

--- vars.tf
+++ vars.tf
@@ -1,48 +1,6 @@
 variable "instances" {
   default = {
-    mongodb = {
-      name = "mongodb"
+    backend = {
+      name = "backend"
       type = "t3.micro"
     }
-    user = {
-      name = "user"
-      type = "t3.small"
-    }
-    frontend = {
-      name = "frontend"
-      type = "t3.small"
-    }
-    cart = {
-      name = "cart"
-      type = "t3.small"
-    }
-    rabbitmq = {
-      name = "rabbitmq"
-      type = "t3.small"
-    }
-    catalogue = {
-      name = "catalogue"
-      type = "t3.small"
-    }
-    rabbitmq = {
-      name = "rabbitmq"
-      type = "t3.small"
-      password = "roboshop123"
-    }
-    mysql = {
-      name = "mysql"
-      type = "t3.small"
-      password = "RoboShop@1"
-    }
-    shipping = {
-      name = "shipping"
-      type = "t3.small"
-      password = "RoboShop@1"
-    }
-    payment = {
-      name = "payment"
-      type = "t3.small"
-      password = "roboshop123"
-    }
-  }
-}
