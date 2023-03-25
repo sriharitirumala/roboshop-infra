557a2fa1d2dee89ad0ae4fb841c63c53a00f2bf3
sriharitirumala <sritirumala30@gmail.com>
2023-03-25 10:33:49 +0530

    [Story-1] adding correct Repos

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git::https://githun.com/sriharitirumala/tf-module-vpc.git"
+  source = "git::https://github.com/sriharitirumala/tf-module-vpc.git"
   env = var.env
 
   for_each = var.vpc
