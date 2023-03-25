726c3d6a475d2252d8bd31143f9b9c949d16341c
sriharitirumala <sritirumala30@gmail.com>
2023-03-25 21:02:48 +0530

    adding vpc subnets, routes, route table associations

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@ module "vpc" {
   source = "git::https://github.com/sriharitirumala/tf-module-vpc.git"
   env = var.env
   tags = var.tags
+  default_route_tags = var.default_route_table
+  default_vpc_id = var.default_vpc_id
 
   for_each = var.vpc
   vpc_cidr = each.value ["vpc_cidr"]
--- terraform.tfvars
+++ terraform.tfvars
@@ -3,5 +3,7 @@ tags ={
   app_name = "roboshop"
   qowner = "ecom_bu"
   cost_centre = 1001
-
 }
+
+default_vpc_id = "vpc-0559526174e73c9c6"
+default_route_table = "rtb-0fdd29ff0a25af1d5"
