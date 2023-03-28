e2c31412817651c158c2ed80a6a4fab2c6fe5932
sriharitirumala <sritirumala30@gmail.com>
2023-03-28 16:28:42 +0530

    adding vpc subnets, routes, route table associations

--- main.tf
+++ main.tf
@@ -40,7 +40,8 @@ module "rds" {
   engine_version                  = each.value ["engine_version"]
   backup_retention_period         = each.value ["backup_retention_period"]
   preferred_backup_window         = each.value ["preferred_backup_window"]
-  instance_class                  = each.value ["no_of_instances"]
+  instance_class                  = each.value ["instance_class"]
+  no_of_instances                 = each.value ["no_of_instances"]
 }
 
 
