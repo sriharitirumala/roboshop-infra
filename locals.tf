68c18ecc6122cf85fdb608f68a84a313737c4b94
sriharitirumala <sritirumala30@gmail.com>
2023-03-28 10:31:41 +0530

    adding vpc subnets, routes, route table associations

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
-locals{
+locals {
   ## Private Subnets
-  private_subnets_ids = { for k, v in module.vpc["main"].private_subnets : k ==> v.id }
+  private_subnet_ids = [for k, v in module.vpc["main"].private_subnets : v.id]
 }
