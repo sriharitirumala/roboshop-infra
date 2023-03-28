b6a2d2dfc23e5554053e0830e71086b76c05afc2
sriharitirumala <sritirumala30@gmail.com>
2023-03-28 11:40:59 +0530

    adding vpc subnets, routes, route table associations

--- locals.tf
+++ locals.tf
@@ -2,12 +2,9 @@ locals {
   ## Private Subnets
   #private_subnet_ids = { for k, v in module.vpc["main"].private_subnets : k => tomap({ "id" = v.id, "availability_zone" = v.availability_zone }) }
   #private_subnet_ids = [for k, v in module.vpc["main"].private_subnets : v.id]
-  subnet_ids = {
-    db     = tolist([module.vpc["main"].private_subnets["db-az1"].id, module.vpc["main"].private_subnets["db-az2"].id])
-    app    = tolist([module.vpc["main"].private_subnets["app-az1"].id, module.vpc["main"].private_subnets["app-az2"].id])
-    web    = tolist([module.vpc["main"].private_subnets["web-az1"].id, module.vpc["main"].private_subnets["web-az2"].id])
-    public = tolist([module.vpc["main"].public_subnets["public-az1"].id, module.vpc["main"].public_subnets["public-az2"].id])
+    db_subnet_ids     = tolist([module.vpc["main"].private_subnets["db-az1"].id, module.vpc["main"].private_subnets["db-az2"].id])
+    app_subnet_ids    = tolist([module.vpc["main"].private_subnets["app-az1"].id, module.vpc["main"].private_subnets["app-az2"].id])
+    web_subnet_ids    = tolist([module.vpc["main"].private_subnets["web-az1"].id, module.vpc["main"].private_subnets["web-az2"].id])
+    public_subnet_ids = tolist([module.vpc["main"].public_subnets["public-az1"].id, module.vpc["main"].public_subnets["public-az2"].id])
   }
 
-
-}
