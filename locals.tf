b3f2a9c02137610f4a1f0d125f918cf6c9cfc4a6
sriharitirumala <sritirumala30@gmail.com>
2023-03-29 18:02:38 +0530

    adding vpc subnets, routes, route table associations

--- locals.tf
+++ locals.tf
@@ -8,6 +8,9 @@ locals {
     web    = tolist([module.vpc["main"].private_subnets["web-az1"].id, module.vpc["main"].private_subnets["web-az2"].id])
     public = tolist([module.vpc["main"].public_subnets["public-az1"].id, module.vpc["main"].public_subnets["public-az2"].id])
   }
+  db     = tolist([module.vpc["main"].private_subnets["db-az1"].id, module.vpc["main"].private_subnets["db-az2"].id])
+  app    = tolist([module.vpc["main"].private_subnets["app-az1"].id, module.vpc["main"].private_subnets["app-az2"].id])
+  web    = tolist([module.vpc["main"].private_subnets["web-az1"].id, module.vpc["main"].private_subnets["web-az2"].id])
 
 }
 
