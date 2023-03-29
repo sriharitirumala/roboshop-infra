fc706333a4618b00ef39ec729377a5e3d0ef225f
sriharitirumala <sritirumala30@gmail.com>
2023-03-29 11:53:22 +0530

    adding vpc subnets, routes, route table associations

--- vars.tf
+++ vars.tf
@@ -3,7 +3,6 @@ variable "tags" {}
 variable "default_vpc_id"{}
 variable "default_route_table"{}
 variable "vpc" {}
-variable "db_subnet_ids" {}
 variable "docdb" {}
 variable "rds" {}
 variable "elasticache" {}
