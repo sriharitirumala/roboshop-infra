284587c47481fc5f4376c9a91f35c3fbd6fa0a53
sriharitirumala <sritirumala30@gmail.com>
2023-04-07 14:32:37 +0530

    adding vpc subnets, routes, route table associations

--- main.tf
+++ main.tf
@@ -147,7 +147,7 @@ resource "aws_spot_instance_request" "load-runner" {
   ami                    = data.aws_ami.ami.id
   instance_type          = "t3.medium"
   wait_for_fulfillment   = true
-  vpc_security_group_ids = [allow-all]
+  vpc_security_group_ids = ["allow-all"]
 
   tags = merge(
     var.tags,
