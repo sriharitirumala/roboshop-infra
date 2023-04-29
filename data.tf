a83ed861ada0df7e612cba647c2e75886a4f750f
sriharitirumala <sritirumala30@gmail.com>
2023-04-29 12:07:41 +0530

    Ammend by SrihariHUB@gmail.com

--- data.tf
+++ data.tf
@@ -3,14 +3,6 @@ data "aws_ssm_parameter" "ssh_pass" {
 }
 
 
-
-data "aws_ami" "ami" {
-  most_recent = true
-  name_regex  = "ami-02c073b6f8c9e1803"
-  owners      = ["self"]
-}
-
-
 #data "aws_ami" "ami" {
 #  most_recent = true
 #  name_regex  = "devops-practice-with-ansible"
