b1c10be7acd57e65eee21f70a49aa4b81134fa0d
sriharitirumala <sritirumala30@gmail.com>
2023-04-29 11:47:41 +0530

    Ammend by SrihariHUB@gmail.com

--- main.tf
+++ main.tf
@@ -178,33 +178,34 @@ resource "null_resource" "load-gen" {
 }
 
 
-#
-#module "minikube" {
-#  source = "github.com/scholzj/terraform-aws-minikube"
-#  aws_region          = "us-east-1"
-#  cluster_name        = "minikube"
-#  aws_instance_type   = "t3.medium"
-#  ssh_public_key      = "~/.ssh/id_rsa.pub"
-#  aws_subnet_id       = lookup(local.subnet_ids, "public", null)[0]
-#  hosted_zone         = "devopsb71services.site"
-#  hosted_zone_private = false
-#  tags = {
-#    Name = "Minikube"
-#  }
-#  addons = [
-#    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
-#    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
-#    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
-#    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
-#  ]
-#}
-#output "MINIKUBE_SERVER" {
-#  value = "ssh centos@${module.minikube.public_ip}"
-#}
-#output "KUBE_CONFIG" {
-#  value = "scp centos@${module.minikube.public_ip}:/home/centos/kubeconfig ~/.kube/config"
-#}
-#
-#
+
+module "minikube" {
+  source = "github.com/scholzj/terraform-aws-minikube"
+  aws_region          = "us-east-1"
+  cluster_name        = "minikube"
+  aws_instance_type   = "t3.medium"
+  ssh_public_key      = "~/.ssh/id_rsa.pub"
+  aws_subnet_id       = lookup(local.subnet_ids, "public", null)[0]
+  hosted_zone         = "devopsb71services.site"
+  hosted_zone_private = false
+  ami_image_id        = ami-02c073b6f8c9e1803
+  tags = {
+    Name = "Minikube"
+  }
+  addons = [
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/storage-class.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/heapster.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml"
+  ]
+}
+output "MINIKUBE_SERVER" {
+  value = "ssh centos@${module.minikube.public_ip}"
+}
+output "KUBE_CONFIG" {
+  value = "scp centos@${module.minikube.public_ip}:/home/centos/kubeconfig ~/.kube/config"
+}
+
+
 
 
