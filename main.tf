72b442fef5bf4f724a6b9a6da0376a4829b47b0d
sriharitirumala <sritirumala30@gmail.com>
2023-05-04 15:44:05 +0530

    update to eks

--- main.tf
+++ main.tf
@@ -205,7 +205,15 @@ output "elb" {
 # }
 
 
-
+module "eks" {
+  source             = "github.com/r-devops/tf-module-eks"
+  ENV                = var.env
+  PRIVATE_SUBNET_IDS = lookup(local.subnet_ids, "app", null)
+  PUBLIC_SUBNET_IDS  = lookup(local.subnet_ids, "public", null)
+  DESIRED_SIZE       = 2
+  MAX_SIZE           = 2
+  MIN_SIZE           = 2
+}
 
 
 
